0b334d0e32b1a8ac69c27a1011ed00abe0e309db
Arfa Adnan <arfaadnan0@gmail.com>
2023-09-18 07:29:57 +0500

    home+classtask

---
+++ app.js
@@ -0,0 +1,444 @@
+// *************************chapter 17-20(Home Assigment)*****************************
+
+// Question 1
+// for (let i = 0; i < 10; i++) {
+// }
+// Question 2
+
+// for (let i = 0; i <= 12; i++) {
+    
+// }
+// Question 3
+// for (var i = 0; i <= 4; i++)
+
+// Question 4
+// for (let count = 0; count < 100; count++) {
+    
+// }
+// Question 5
+// for (let i = 3; i > 0; i--) {
+// }
+
+// Question 6
+// let myArray = [1, 2, 3, 4, 5]; 
+// let numberOfElements = myArray.length;
+
+// Question 7
+
+// let flag = true;
+
+// Question 8
+// let pets = ["dog", "cat", "fish", "bird"];
+// for (let i = 0; i < pets.length; i++) { 
+// }
+
+// Question 9
+
+// for (let i = 0; i < 10; i++) {
+    // if (i === 1) {
+        // alert("The counter is: " + i); 
+        // break; 
+    // }
+// }
+
+// Question 10
+
+// let userNames = ["Ant", "Bee", "cat", "Dog", "Elephant"];
+
+
+// let firstName = prompt("Enter first name");
+
+
+// for (let i = 0; i < userNames.length; i++) {
+    
+//     if (firstName === userNames[i]) {
+//         alert("Welcome, " + firstName); 
+//         break; 
+//     }
+
+    
+//     if (i === userNames.length - 1) {
+//         alert("Please write the correct user name");
+//     }
+// }
+
+// Question 11
+
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+//     if (userInput === list[i]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+// }
+
+// if (!matchFound) {
+//     alert("No match found");
+// }
+
+// Question 12
+
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+//     for (var j = 0; j < secondArr.length; j++) {
+//         console.log(firstArr[i] + secondArr[j]);
+//     }
+// }
+
+
+// *********************************Class Task******************************
+
+
+
+// Question 3
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// Question 4
+
+
+
+// const number = parseInt(prompt("Enter the number for the multiplication table:"));
+// const length = parseInt(prompt("Enter the length of the multiplication table:"));
+
+
+// if (isNaN(number) || isNaN(length)) {
+//     alert("Please enter valid numbers.");
+// } else {
+    
+//     console.log(`Multiplication Table for ${number} (up to ${length} terms):`);
+//     for (let i = 1; i <= length; i++) {
+//         const result = number * i;
+//         console.log(`${number} x ${i} = ${result}`);
+//     }
+// }
+
+// Question 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// Question 6
+
+// // Counting: 1 to 15
+// var counting = "Counting: ";
+// for (var i = 1; i <= 15; i++) {
+//     counting += i + ", ";
+// }
+// console.log(counting);
+
+// // Reverse counting: 10 to 1
+// var reverseCounting = "Reverse counting: ";
+// for (var j = 10; j >= 1; j--) {
+//     reverseCounting += j + ", ";
+// }
+// console.log(reverseCounting);
+
+// // Even: 0 to 20, increment by 2
+// var even = "Even: ";
+// for (var k = 0; k <= 20; k += 2) {
+//     even += k + ", ";
+// }
+// console.log(even);
+
+// // Odd: 1 to 19, increment by 2
+// var odd = "Odd: ";
+// for (var l = 1; l <= 19; l += 2) {
+//     odd += l + ", ";
+// }
+// console.log(odd);
+
+// // Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+// var series = "Series: ";
+// for (var m = 2; m <= 20; m += 2) {
+//     series += (m * 2) + "k, ";
+// }
+// console.log(series);
+
+
+// Question 7
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var userInput = prompt("Enter an item to search:");
+
+// userInput = userInput.toLowerCase();
+
+
+// var itemFound = false;
+
+
+// for (var i = 0; i < A.length; i++) {
+//     if (A[i].toLowerCase() === userInput) {
+//         itemFound = true;
+//         break;
+//     }
+// }
+
+
+// if (itemFound) {
+//     alert(userInput + " is found in the list.");
+// } else {
+//     alert(userInput + " is not found in the list.");
+// }
+
+
+// Question 8
+
+// var A = [24, 53, 78, 91, 12];
+
+// var maxNumber = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] > maxNumber) {
+//         maxNumber = A[i];
+//     }
+// }
+
+// console.log("The largest number in the array is: " + maxNumber);
+
+// Question 9
+
+// var A = [24, 53, 78, 91, 12];
+
+
+// var minNumber = A[0];
+
+
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] < minNumber) {
+//         minNumber = A[i];
+//     }
+// }
+
+
+// console.log("The smallest number in the array is: " + minNumber);
+
+// Question 10
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// ********************************the end*************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
